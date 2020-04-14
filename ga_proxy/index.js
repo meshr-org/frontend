74fec296d70ce1b05fc64836574f3e93eb4186c0
robertsahlin <robert.sahlin@gmail.com>
2020-04-14 23:23:21 +0200

    check

--- ga_proxy/index.js
+++ ga_proxy/index.js
@@ -7,9 +7,8 @@
 
 'use strict';
 
-const mainTopic = process.env.TOPIC || 'tmp';
+const mainTopic = process.env.MAIN_TOPIC || 'ga-proxy';
 const allowOrigin = process.env.ALLOW_ORIGIN || '*';
-//const platform = process.env.PLATFORM || 'CF';
 
 // import the Google Cloud Pubsub client library
 const {PubSub} = require('@google-cloud/pubsub');
@@ -18,9 +17,8 @@ const topic = pubsub.topic(mainTopic);
 
 const TRANSPARENT_GIF_BUFFER = Buffer.from('R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=', 'base64');
 
-const util = require('util');
+//const util = require('util');
 const uuidv4 = require('uuid/v4');
-
 
 const express = require('express');
 var cors = require('cors');
@@ -39,28 +37,27 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
-app.get('/topic/:topicId', apiGet);
-app.post('/topic/:topicId', apiPost);
-app.post('/cookies', apiCookie);
+app.get('/property/:propertyId', apiGet);
+app.post('/property/:propertyId', apiPost);
 app.get('/headers', apiHeaders);
 app.get('/keepalive', apiKeepAlive);
 
 
 
 // Set our GCF handler to our Express app.
-exports.collector = app;
+exports.ga_proxy = app;
 
 async function publish(req, res){
-    var payload = {};
-    payload.body = req.body;
-    payload.query = req.query;
-    payload.headers = req.headers;
+    var beacon = {};
+    beacon.payload = (Object.keys(req.body).length !== 0) ? req.body : req.query;
+    beacon.headers = req.headers;
     var attributes = {
-        topic : req.params.topicId,
+        property : req.params.propertyId,
+        topic : mainTopic,
         timestamp :  new Date().toISOString(),
         uuid : uuidv4()
     };
-    var msgData = Buffer.from(JSON.stringify(payload));
+    var msgData = Buffer.from(JSON.stringify(beacon));
     const messageId = await topic
         .publish(msgData, attributes)
         .catch(function(err) {
@@ -70,27 +67,16 @@ async function publish(req, res){
         console.log(`Message ${messageId} published.`);
 }
 
-async function apiCookie(req, res) {
-    try{
-        (Array.isArray(req.body) ? req.body : [req.body])
-            .forEach(cookie => {res.cookie(cookie.name, cookie.value, cookie.options)});
-        res.status(204).end();
-    }catch(error) {
-        console.error(error);
-        res.status(400).end(`error when rewriting cookies`);
-    }
-}
-
 async function apiGet(req, res) {
-    if(req.params.topicId !== undefined){
+    if(req.params.propertyId !== undefined){
         await publish(req, res);
         if(!res.headersSent){
             res.writeHead(200, { 'Content-Type': 'image/gif' });
             res.end(TRANSPARENT_GIF_BUFFER, 'binary');
         }
     }else{
-        console.error("topic path param undefined");
-        res.status(400).end('Topic path param undefined. pattern should be https://host/topic/:topicID');    
+        console.error("property path param undefined");
+        res.status(400).end('Property path param undefined. pattern should be https://host/property/:propertyId');    
     }
 }
 
@@ -103,13 +89,13 @@ async function apiKeepAlive(req, res) {
 }
 
 async function apiPost(req, res) {
-    if(req.params.topicId !== undefined){
+    if(req.params.propertyId !== undefined){
         await publish(req, res);
         if(!res.headersSent){
             res.status(204).end();
         }
     }else{
-        console.error("topic path param undefined");
-        res.status(400).end('Topic path param undefined. pattern should be https://host/topic/:topicID');    
+        console.error("property path param undefined");
+        res.status(400).end('Property path param undefined. pattern should be https://host/property/:propertyId');    
     }
 }
