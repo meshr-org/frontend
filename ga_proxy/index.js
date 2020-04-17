29c3b7bf4bb546338759e7391bb0e9afee2b0a17
robertsahlin <robert.sahlin@gmail.com>
2020-04-17 17:25:15 +0200

    topic and backup topic

--- ga_proxy/index.js
+++ ga_proxy/index.js
@@ -8,28 +8,29 @@
 'use strict';
 
 const mainTopic = process.env.MAIN_TOPIC || 'ga-proxy';
-const allowOrigin = process.env.ALLOW_ORIGIN || '*';
+const backupTopic = process.env.BACKUP_TOPIC || 'backup';
+const allowOrigins = process.env.ALLOW_ORIGINS || '*';
 
 // import the Google Cloud Pubsub client library
 const {PubSub} = require('@google-cloud/pubsub');
 const pubsub = new PubSub();
 const topic = pubsub.topic(mainTopic);
-
+const backup = pubsub.topic(backupTopic);
 const TRANSPARENT_GIF_BUFFER = Buffer.from('R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=', 'base64');
-
-//const util = require('util');
 const uuidv4 = require('uuid/v4');
 const anonymize = require('ip-anonymize');
-
 const express = require('express');
+
 var cors = require('cors');
 
 // Create an Express object and routes (in order)
 const app = express();
 app.set('trust proxy', true);
+
+// CORS
 var corsOptions = {
   origin: function (origin, callback) {
-    if (allowOrigin === '*' || allowOrigin.split(',').indexOf(origin) !== -1 || !origin) {
+    if (allowOrigins === '*' || allowOrigins.split(',').indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
@@ -38,12 +39,12 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+
+// Routing
 app.get('/property/:propertyId', apiGet);
 app.post('/property/:propertyId', apiPost);
 app.get('/headers', apiHeaders);
 app.get('/keepalive', apiKeepAlive);
-
-
 
 // Set our GCF handler to our Express app.
 exports.ga_proxy = app;
@@ -52,8 +53,12 @@ async function publish(req, res){
     var beacon = {};
     beacon.body = req.body;
     beacon.query = req.query;
-    //beacon.headers = req.headers;
-    beacon.headers = ['x-forwarded-for', 'user-agent','x-appengine-city','x-appengine-citylatlong','x-appengine-country','x-appengine-region'].reduce(function(o, k) { o[k] = req.headers[k]; return o; }, {});
+    
+    // Extract selected headers
+    beacon.headers = ['x-forwarded-for', 'user-agent','x-appengine-city','x-appengine-citylatlong','x-appengine-country','x-appengine-region']
+        .reduce(function(o, k) { o[k] = req.headers[k]; return o; }, {});
+    
+    // Anonymize IP
     beacon.headers['x-forwarded-for'] = anonymize(beacon.headers['x-forwarded-for']);
     var attributes = {
         property : req.params.propertyId,
@@ -62,13 +67,13 @@ async function publish(req, res){
         uuid : uuidv4()
     };
     var msgData = Buffer.from(JSON.stringify(beacon));
-    const messageId = await topic
-        .publish(msgData, attributes)
-        .catch(function(err) {
-            console.error(err.message);
-            res.status(400).end(`error when publishing data object to pubsub`); 
-        });
-        console.log(`Message ${messageId} published.`);
+    
+    // Publish to topic and backup
+    await Promise.all([topic,backup].map(currentTopic => currentTopic.publish(msgData, attributes)))
+    .catch(function(err) {
+        console.error(err.message);
+        res.status(400).end(`error when publishing data object to pubsub`);
+    });
 }
 
 async function apiGet(req, res) {
@@ -84,10 +89,12 @@ async function apiGet(req, res) {
     }
 }
 
+// Respond with headers
 async function apiHeaders(req, res) {
     res.json(req.headers).end()
 }
 
+// Schedule requests to keep instance warm
 async function apiKeepAlive(req, res) {
     res.status(204).end();
 }
