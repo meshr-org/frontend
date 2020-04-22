8c693fc9fbb10048154fa4c465a9712b34d2fc7d
robertsahlin <robert.sahlin@gmail.com>
2020-04-22 12:00:09 +0200

    working generic cf collector

--- collector/index.js
+++ collector/index.js
@@ -7,46 +7,36 @@
 
 'use strict';
 
-const apiKeys = process.env.API_KEYS;
-const allowOrigins = process.env.ALLOW_ORIGINS;
-const headers = process.env.HEADERS; // 'x-forwarded-for,user-agent,x-appengine-city,x-appengine-citylatlong,x-appengine-country,x-appengine-region';
-
-// import the Google Cloud Pubsub client library
+// imports
 const {PubSub} = require('@google-cloud/pubsub');
-const pubsub = new PubSub();
-//const topic = pubsub.topic(mainTopic);
-//const backup = pubsub.topic(backupTopic);
-const TRANSPARENT_GIF_BUFFER = Buffer.from('R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=', 'base64');
 const uuidv4 = require('uuid/v4');
-//const anonymize = require('ip-anonymize');
 const express = require('express');
-
 var cors = require('cors');
 
-// Create an Express object and routes (in order)
-const app = express();
-app.set('trust proxy', true);
+const apiKeys = process.env.API_KEYS;
+const allowOrigins = process.env.ALLOW_ORIGINS || '';
+const headers = process.env.HEADERS; // ex. 'x-forwarded-for,user-agent,x-appengine-city,x-appengine-citylatlong,x-appengine-country,x-appengine-region'
+const pubsub = new PubSub();
 
 // CORS
+// client request -> check allowed origins
+// server request -> check api_key
 var corsOptionsDelegate = function (req, callback) {
     var corsOptions;
-    // client -> check allowed origins, server -> check api_key
     const origin = req.header('Origin');
-    //if (allowOrigins.split(',').indexOf(origin) !== -1 || (!origin && apiKeys != undefined && apiKeys.split(',').indexOf(req.query.api_key) !== -1)) {
-        if (true){
+    if (allowOrigins.split(',').indexOf(origin) !== -1 || (!origin && apiKeys != undefined && apiKeys.split(',').indexOf(req.query.api_key) !== -1)) {
         corsOptions = { origin: true }
         callback(null, corsOptions)
     } else {
-      //corsOptions = { origin: false }
       callback(new Error('Not allowed by CORS'));
     }
 }
 
-// Pre-flight
-app.options('*', cors(corsOptionsDelegate));
-// Routing. ex. /namespace/com.google.analytics.v1/name/Hit -> topic = com-google-analytics-v1-hit-collector
-//app.post('/namespace/:namespace/name/:name', cors(corsOptionsDelegate), apiPost);
-app.post('/namespace/:namespace(*)/name/:name/', apiPost);
+// Create an Express object and routes (in order)
+const app = express();
+app.set('trust proxy', true);
+app.options('*', cors(corsOptionsDelegate)); // Pre-flight
+app.post('/namespace/:namespace/name/:name', cors(corsOptionsDelegate), apiPost); // namespace/com.google.analytics.v1/name/Hit
 app.get('/headers', cors(corsOptionsDelegate), apiHeaders);
 app.get('/keepalive', cors(corsOptionsDelegate), apiKeepAlive);
 
@@ -54,19 +44,13 @@ app.get('/keepalive', cors(corsOptionsDelegate), apiKeepAlive);
 exports.collector = app;
 
 async function publish(req, res){
-    var topic = req.params.namespace.concat('.', req.params.name);
+    
+    // Pubsub topics to publish message on
+    var topic = req.params.namespace.concat('.', req.params.name, '-collector');
     var topics =[topic];
     if(req.query.backup) topics.push(req.query.backup); // Add backup topic if backup parameter exist in querystring
 
-    var data = {};
-    data.body = req.body;
-    // Extract selected headers
-    console.log('req.query');
-    console.log(req);
-    const headersFilter = req.query.headers;// || headers;
-    
-    data.headers = headersFilter.split(',').reduce(function(o, k) { o[k] = req.headers[k]; return o; }, {});
-
+    // Pubsub message attributes
     var meta = {
         namespace : req.params.namespace,
         name : req.params.name,
@@ -77,28 +61,21 @@ async function publish(req, res){
     var attributes = {...req.query,...meta};
     delete attributes.headers;
     delete attributes.api_key;
-    
-    var msgData = Buffer.from(JSON.stringify(data));
-    
-    // Publish to topics defined by topics query parameter (i.e. &topics=dummy,backup,yet-a-topic)
-    await Promise.all(topics.map(currentTopic => pubsub.topic(currentTopic).publish(msgData, attributes)))
+
+    // Pubsub message body
+    var body = {};
+    body.data = req.body;    
+    const headersFilter = req.query.headers; // Keep selected headers
+    body.headers = headersFilter.split(',').reduce(function(o, k) { o[k] = req.headers[k]; return o; }, {});
+    var msgBody = Buffer.from(JSON.stringify(body));
+
+    // Publish to topics
+    let messageIds = await Promise.all(topics.map(currentTopic => pubsub.topic(currentTopic).publish(msgBody, attributes)))
     .catch(function(err) {
         console.error(err.message);
         res.status(400).end(`error when publishing data object to pubsub`);
     });
-}
-
-async function apiGet(req, res) {
-    if(req.params.propertyId !== undefined){
-        await publish(req, res);
-        if(!res.headersSent){
-            res.writeHead(200, { 'Content-Type': 'image/gif' });
-            res.end(TRANSPARENT_GIF_BUFFER, 'binary');
-        }
-    }else{
-        console.error("property path param undefined");
-        res.status(400).end('Property path param undefined. pattern should be https://host/property/:propertyId');    
-    }
+    //console.log(messageIds);
 }
 
 // Respond with headers
@@ -106,19 +83,20 @@ async function apiHeaders(req, res) {
     res.json(req.headers).end()
 }
 
-// Schedule requests to keep instance warm
+// Schedule requests to this endpoint keep instance warm
 async function apiKeepAlive(req, res) {
     res.status(204).end();
 }
 
+// Collect request (POST) and publish data on pubsub
 async function apiPost(req, res) {
-    //if(req.params.propertyId !== undefined){
+    if(req.params.namespace !== undefined && req.params.name !== undefined){ // Check if required params exist
         await publish(req, res);
         if(!res.headersSent){
             res.status(204).end();
         }
-    /*}else{
-        console.error("property path param undefined");
-        res.status(400).end('Property path param undefined. pattern should be https://host/property/:propertyId');    
-    }*/
+    }else{
+        console.error("Path param undefined");
+        res.status(400).end('Path param undefined. Pattern should be https://host/namespace/:namespace(*)/name/:name/');    
+    }
 }
